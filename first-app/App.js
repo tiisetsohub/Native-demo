9a0fc2368884a4f9abacde526e313142dc6d6b57
tiisetso <tiimojalefa@gmail.com>
2022-07-17 00:46:21 +0200

    todo

--- first-app/App.js
+++ first-app/App.js
@@ -4,28 +4,64 @@ import React, { useState, useReducer } from 'react'
 const reducer = (state, action)=> {
   switch (action.type) {
     case 'add':
-      return { count : state.count + 1, name : 'ADD'}
-    case 'minus':
-      return {...state, count: state.count - 1}
+      return [...state, newItem(action.payload.name)]
+    case 'remove':
+      return state.filter(state => state.name !== action.payload.name)
     default:
       return state
   }
 }
 
+const newItem = (name) => {
+  return {id: name, name: name, complete: false}
+}
+
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, {count: 0, name: 'add'});
+  const [state, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
 
   return (
     <View style = {styles.body}>
-      <View style = {styles.mid}>
-        <Pressable style={styles.button} onPress={() => dispatch({type : 'minus'})}>
-          <Text>MINUS</Text>
-        </Pressable>
-        <Text style={styles.text}>{state.count}</Text>
-        <Pressable style={styles.button} onPress = {() => dispatch({type : 'add'})}>
-          <Text>{state.name}</Text>
-        </Pressable>
-      </View>
+      <TextInput
+      value = {name}
+      placeholder ='add note'
+      style = {styles.input}
+      onChangeText={value => setName(value)}
+      />
+      <Pressable 
+      style = {styles.button}
+      onPress = {() => 
+        {
+          dispatch({type : 'add', payload : {name: name}})
+          setName('')
+
+        }
+      }
+      >
+        <Text>ADD</Text>
+      </Pressable>
+      {state.map((item) => 
+        {
+          return (
+            <View style = {styles.thing}>
+              <View style = {styles.leftthing}>
+                <Text>{item.name}</Text>
+              </View>
+              <View style = {styles.rightthing}>
+                <Pressable
+                  onPress={() => {
+                    dispatch({ type: 'remove', payload: { name: item.name } })
+                    setName('')
+                   }
+                  }>
+                  <Text>🗑</Text>
+                </Pressable>
+              </View>
+            </View>
+          )
+        }
+      )}
+
     </View>
   );
 }
@@ -34,24 +70,42 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  input: {
+    marginTop : 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    margin: 10,
+    borderRadius: 2,
+    padding: 10,
+    width: 320,
+    textAlign: 'center',
+  },
+  button: {
+    borderRadius: 2,
+    backgroundColor: '#b63ddb',
+    width: 320,
+    height: 30,
+    alignItems: 'center',
     justifyContent: 'center',
-    alignItems: 'center',
+    marginBottom: 20
   },
-  mid: {
-    flexDirection : 'row'
+  thing: {
+    borderRadius: 2,
+    padding: 10,
+    backgroundColor : '#ffbbdd',
+    width: 320,
+    textAlign: 'center',
+    justifyContent : 'space-between',
+    flexDirection: 'row',
+    marginBottom : 10
   },
-  text: {
-    width : 80,
-    fontSize : 20,
-    textAlign : 'center',
-  },
-  button : {
-    borderWidth : 1,
-    borderColor: '#b63ddb',
-    borderRadius : 2,
-    width : 150,
-    height : 30,
-    alignItems: 'center',
-    justifyContent : 'center',
+  leftthing: {
+    alignSelf : 'left'
+  }, 
+  rightthing: {
+    alignSelf: 'right'
   }
 });
